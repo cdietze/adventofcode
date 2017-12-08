9bbddf82811c8bfa0e84372c5616d2337c3b2404
Christoph Dietze <christoph.dietze@kurzdigital.com>
2017-12-08 22:41:27 +0100

    Refactor advent 3

--- 2017/src/main/kotlin/advent03/Advent03.kt
+++ 2017/src/main/kotlin/advent03/Advent03.kt
@@ -7,6 +7,7 @@ import kotlin.test.assertEquals
 fun main(args: Array<String>) {
     val input = 325489
     println("Result part 1: ${manhattan(input)}")
+    println("Result part 1 version 2: ${manhattan2(input)}")
     println("Result part 2: ${accValues().find { it.second > input }!!.second}")
 }
 
@@ -23,6 +24,8 @@ fun squareIndex(loc: Int): Int {
 
 fun manhattan(loc: Int): Int = manhattanImpl(loc - 1)
 
+fun manhattan2(loc: Int): Int = spiral().elementAt(loc - 1).let { it.x.absoluteValue + it.y.absoluteValue }
+
 fun manhattanImpl(loc: Int): Int {
     val square = squareIndex(loc)
     val maxSquareLoc = maxSquareLoc(square)
@@ -37,9 +40,8 @@ fun manhattanImpl(loc: Int): Int {
 
 data class Point(val x: Int, val y: Int)
 
-fun accValues(): Sequence<Pair<Point, Int>> = buildSequence {
+fun accValues(): Sequence<Pair<Point, Int>> {
     val map: MutableMap<Point, Int> = mutableMapOf<Point, Int>().withDefault { 0 }
-
     fun calcAndStore(p: Point): Int {
         val sum = map.getValue(p.copy(x = p.x + 1)) +
                 map.getValue(p.copy(x = p.x + 1, y = p.y + 1)) +
@@ -52,34 +54,26 @@ fun accValues(): Sequence<Pair<Point, Int>> = buildSequence {
         map[p] = sum
         return sum
     }
+    map[Point(0, 0)] = 1
+    return sequenceOf(Pair(Point(0, 0), 1)) + spiral().drop(1).map { Pair(it, calcAndStore(it)) }
+}
 
-    var pos = Point(0, 0)
-    map[pos] = 1
-    yield(Pair(pos, 1))
+fun spiral(): Sequence<Point> = buildSequence {
+    var x = 0
+    var y = 0
+    yield(Point(x, y))
     var steps = 1
     while (true) {
         // move steps right
-        (1..steps).forEach {
-            pos = pos.copy(x = pos.x + 1)
-            yield(Pair(pos, calcAndStore(pos)))
-        }
+        (1..steps).forEach { yield(Point(++x, y)) }
         // move steps up
-        (1..steps).forEach {
-            pos = pos.copy(y = pos.y + 1)
-            yield(Pair(pos, calcAndStore(pos)))
-        }
+        (1..steps).forEach { yield(Point(x, ++y)) }
         // inc steps
         steps += 1
         // move steps left
-        (1..steps).forEach {
-            pos = pos.copy(x = pos.x - 1)
-            yield(Pair(pos, calcAndStore(pos)))
-        }
+        (1..steps).forEach { yield(Point(--x, y)) }
         // move steps down
-        (1..steps).forEach {
-            pos = pos.copy(y = pos.y - 1)
-            yield(Pair(pos, calcAndStore(pos)))
-        }
+        (1..steps).forEach { yield(Point(x, --y)) }
         // inc steps and repeat
         steps += 1
     }
