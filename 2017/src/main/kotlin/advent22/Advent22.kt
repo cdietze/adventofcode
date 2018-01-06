9767227869a43313ca6f538745efe454961bdfdf
Christoph Dietze <christoph.dietze@kurzdigital.com>
2018-01-06 22:03:16 +0100

    Solve advent 22 part 2

--- 2017/src/main/kotlin/advent22/Advent22.kt
+++ 2017/src/main/kotlin/advent22/Advent22.kt
@@ -1,5 +1,6 @@
 package advent22
 
+import advent22.NodeState.*
 import java.io.File
 import kotlin.test.fail
 
@@ -30,12 +31,45 @@ fun State.step(): State {
     return copy(pos = pos.move(newDir), dir = newDir, infectionCount = newCount)
 }
 
+enum class NodeState {
+    CLEAN,
+    WEAKENED,
+    INFECTED,
+    FLAGGED
+}
+
+fun NodeState.next() = when (this) {
+    CLEAN -> WEAKENED
+    WEAKENED -> INFECTED
+    INFECTED -> FLAGGED
+    FLAGGED -> CLEAN
+}
+
+fun NodeState.nextDir(dir: Direction) = when (this) {
+    CLEAN -> dir.left()
+    WEAKENED -> dir
+    INFECTED -> dir.right()
+    FLAGGED -> dir.right().right()
+}
+
+data class State2(val nodes: MutableMap<Point, NodeState>, val pos: Point = Point(0, 0), val dir: Direction = UP, val infectionCount: Int = 0)
+
+fun State2.step(): State2 {
+    val nodeState = nodes.getOrDefault(pos, CLEAN)
+    val newDir = nodeState.nextDir(dir)
+    val newCount = if (nodeState == WEAKENED) infectionCount + 1 else infectionCount
+    nodes[pos] = nodeState.next()
+    return copy(pos = pos.move(newDir), dir = newDir, infectionCount = newCount)
+}
+
 fun main(args: Array<String>) {
     val input = mutableSetOf<Point>()
     File("src/main/kotlin/advent22/input.txt").readLines().forEachIndexed { y, row ->
         row.forEachIndexed { x, c -> if (c == '#') input.add(Point(x, y)) }
     }
     val start = Point(12, 12)
-    val state = (0 until 10000).fold(State(input.toMutableSet(), pos = start), { acc, i -> acc.step() })
+    val state = (0 until 10000).fold(State(input.toMutableSet(), pos = start), { acc, _ -> acc.step() })
     println("Result part 1: ${state.infectionCount}")
+    val state2 = (0 until 10000000).fold(State2(input.map { Pair(it, INFECTED) }.toMap().toMutableMap(), pos = start), { acc, _ -> acc.step() })
+    println("Result part 2: ${state2.infectionCount}")
 }
