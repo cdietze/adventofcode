ff568e621bcbfad994785ea7f3094a5dda5f3df2
Christoph Dietze <git@cdietze.de>
2018-01-07 22:28:05 +0100

    Add raw high-level version for advent 23 part 2

--- 2017/src/main/kotlin/advent23/Advent23.kt
+++ 2017/src/main/kotlin/advent23/Advent23.kt
@@ -26,8 +26,80 @@ tailrec fun State.run(instructions: List<Instruction>): State {
 
 fun getValue(s: String, mem: Map<String, Int>) = s.toIntOrNull() ?: mem.getOrDefault(s, 0)
 
+fun solve2(): Int {
+    val a = 1
+    var c = 0
+    var d = 0
+    var e = 0
+    var f = 0
+    var g = 0
+    var h = 0
+//    set b 99
+    var b = 99
+//    set c b
+//    mul b 100
+    b *= 100
+//    sub b -100000
+    b += 100000
+//    set c b
+    c = b
+//    sub c -17000
+    c += 17000
+    do {
+//    set f 1
+        f = 1
+//    set d 2
+        d = 2
+        do {
+//    set e 2
+            e = 2
+            do {
+//    set g d
+                g = d
+//    mul g e
+                g *= e
+//    sub g b
+                g -= b
+//    jnz g 2
+//    set f 0
+                if (g == 0) f = 0
+//    sub e -1
+                e += 1
+//    set g e
+                g = e
+//    sub g b
+                g -= b
+//    jnz g -8
+            } while (g != 0)
+//    sub d -1
+            d += 1
+//    set g d
+            g = d
+//    sub g b
+            g -= b
+//    jnz g -13
+        } while (g != 0)
+//    jnz f 2
+//    sub h -1
+        if (f == 0) h += 1
+//    set g b
+        g = b
+//    sub g c
+        g -= c
+//    jnz g 2
+//    jnz 1 3
+        if (g == 0) return h
+//    sub b -17
+        b += 17
+//    jnz 1 -23
+        println("b:$b,c:$c,d:$d,e:$e,f:$f,g:$g,h:$h")
+    } while (true)
+}
+
 fun main(args: Array<String>) {
     val instructions = File("src/main/kotlin/advent23/input.txt").readLines().map { it.split(" ") }
     val result = State().run(instructions)
     println("Result part 1: ${result.mulCounter}")
+    val result2 = solve2()
+    println("result2: $result2")
 }
