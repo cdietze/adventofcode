d7f705e1a73013b58d52e6b8ed639f2c52c1f2d8
Christoph Dietze <git@cdietze.de>
2018-12-11 00:14:17 +0100

    Solve day 8 part 2

--- 2018/src/main/kotlin/advent2018/day08/Day08.kt
+++ 2018/src/main/kotlin/advent2018/day08/Day08.kt
@@ -8,7 +8,7 @@ val inputFile = File("src/main/kotlin/advent2018/day08/input.txt")
 
 fun main(args: Array<String>) {
     println("Result part 1: ${solvePart1()}")
-//    println("Result part 2: ${solvePart2()}")
+    println("Result part 2: ${solvePart2()}")
 }
 
 data class Node(val children: List<Node>, val metadata: List<Int>)
@@ -37,6 +37,13 @@ fun metadataParser(count: Int): Parser<List<Int>> = Rule("Metadata") {
 
 fun Node.metadataSum(): Int = children.sumBy { it.metadataSum() } + metadata.sum()
 
+fun Node.value(): Int = if (children.isEmpty()) metadata.sum() else
+    metadata.map { index -> children.getOrNull(index - 1)?.value() ?: 0 }.sum()
+
 fun solvePart1(): Int {
     return inputFile.readText().let { nodeParser.parse(it).getOrFail().value }.metadataSum()
+}
+
+fun solvePart2(): Int {
+    return inputFile.readText().let { nodeParser.parse(it).getOrFail().value }.value()
 }
