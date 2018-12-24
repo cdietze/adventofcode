81b85a6230d726acadcca470acc31f87e05b8827
Christoph Dietze <git@cdietze.de>
2018-12-24 21:55:47 +0100

    Solve day 14 part 2, refactored to string-based data structure

--- 2018/src/main/kotlin/advent2018/day14/Day14.kt
+++ 2018/src/main/kotlin/advent2018/day14/Day14.kt
@@ -1,29 +1,38 @@
 package advent2018.day14
 
-val input = 513401
-//val input = 9
+const val input = "513401"
+
+const val zeroCharValue = '0'.toInt()
 
 fun main(args: Array<String>) {
     println("Result part 1: ${solvePart1()}")
-//    println("Result part 2: ${solvePart2()}")
+    println("Result part 2: ${solvePart2()}")
 }
 
-data class State(val recipes: MutableList<Int>, val firstIndex: Int, val secondIndex: Int)
+data class State(val recipes: StringBuilder, val firstIndex: Int, val secondIndex: Int)
 
 fun State.step(): State {
-    val firstVal = recipes[firstIndex]
-    val secondVal = recipes[secondIndex]
-    val sum = firstVal + secondVal
-    if (sum / 10 == 1) recipes.add(1)
-    recipes.add(sum % 10)
+    val firstVal: Int = recipes[firstIndex].toInt() - zeroCharValue
+    val secondVal: Int = recipes[secondIndex].toInt() - zeroCharValue
+    recipes.append(firstVal + secondVal)
     return copy(
-        firstIndex = (firstIndex + firstVal + 1) % recipes.size,
-        secondIndex = (secondIndex + secondVal + 1) % recipes.size
+        firstIndex = (firstIndex + firstVal + 1) % recipes.length,
+        secondIndex = (secondIndex + secondVal + 1) % recipes.length
     )
 }
 
 fun solvePart1(): String {
-    var state = State(mutableListOf(3, 7), firstIndex = 0, secondIndex = 1)
-    while (state.recipes.size < input + 10) state = state.step()
-    return state.recipes.drop(input).take(10).joinToString("")
+    val inputAsInt = input.toInt()
+    var state = State(StringBuilder("37"), firstIndex = 0, secondIndex = 1)
+    while (state.recipes.length < inputAsInt + 10) state = state.step()
+    return state.recipes.drop(inputAsInt).take(10).toString()
+}
+
+fun solvePart2(): Int {
+    tailrec fun loop(state: State): Int {
+        val index = state.recipes.takeLast(input.length + 2).indexOf(input)
+        return if (index >= 0) state.recipes.indexOf(input)
+        else loop(state.step())
+    }
+    return loop(State(StringBuilder("37"), firstIndex = 0, secondIndex = 1))
 }
