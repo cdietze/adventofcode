392d2c57132985582bf81c9e70591264cb0dd1b0
Christoph Dietze <git@cdietze.de>
2019-12-01 14:19:28 +0100

    Solve 2019 day 1 part 1

---
+++ 2019/README.md
@@ -0,0 +1,9 @@
+# My Solutions to [Advent of Code 2019](https://adventofcode.com/2019)
+
+To run solution for day `x`, call
+
+    ./gradlew run -Pday=x
+
+E.g. to run day 1, call
+
+    ./gradlew run -Pday=01
---
+++ 2019/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.3.60"
+
+    // Apply the application to add support for building a CLI application
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+application {
+    val day = project.findProperty("day") ?: "01"
+    println("Running day $day")
+    mainClassName = "advent2019.day$day.MainKt"
+}
