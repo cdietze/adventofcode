d2ff73c5c0643e94727f6087ff6bcf17535e2134
Christoph Dietze <christoph.dietze@kurzdigital.com>
2019-12-07 23:59:04 +0100

    Solve day 5 part 2

--- 2019/src/main/kotlin/advent2019/day05/Main.kt
+++ 2019/src/main/kotlin/advent2019/day05/Main.kt
@@ -4,11 +4,17 @@ val inputText = object {}.javaClass.getResource("input.txt").readText()
 
 fun main() {
     println("Result part 1: ${solvePart1()}")
+    println("Result part 2: ${solvePart2()}")
 }
 
 fun solvePart1(): Int {
     val mem = inputText.split(",").map { it.toInt() }.toMutableList()
     return State(mem = mem).run().output.first { it != 0 }
+}
+
+fun solvePart2(): Int {
+    val mem = inputText.split(",").map { it.toInt() }.toMutableList()
+    return State(mem = mem, input = 5).run().output.first { it != 0 }
 }
 
 data class State(
@@ -22,41 +28,74 @@ data class State(
 fun State.run(): State {
     var state = this
     while (!state.done) {
-        // println(this)
+        //println(this)
         state = state.step()
     }
-    // println("DONE\n$this")
+    //println("DONE\n$this")
     return this
 }
 
-fun State.step(): State = when (mem[pc].opcode()) {
-    1 -> { // ADD
-        mem[mem[pc + 3]] = getParam(0) + getParam(1); pc += 4; this
+fun State.step(): State {
+    when (mem[pc].opcode()) {
+        1 -> { // ADD
+            setParam(3, getParam(1) + getParam(2))
+            pc += 4
+        }
+        2 -> { // MUL
+            setParam(3, getParam(1) * getParam(2))
+            pc += 4
+        }
+        3 -> { // INPUT
+            setParam(1, input)
+            pc += 2
+        }
+        4 -> { // OUTPUT
+            output.add(getParam(1))
+            pc += 2
+        }
+        5 -> { // jump-if-true
+            if (getParam(1) != 0) {
+                pc = getParam(2)
+            } else {
+                pc += 3
+            }
+        }
+        6 -> { // jump-if-false
+            if (getParam(1) == 0) {
+                pc = getParam(2)
+            } else {
+                pc += 3
+            }
+        }
+        7 -> { // less-than
+            setParam(3, if (getParam(1) < getParam(2)) 1 else 0)
+            pc += 4
+        }
+        8 -> { // equals
+            setParam(3, if (getParam(1) == getParam(2)) 1 else 0)
+            pc += 4
+        }
+        99 -> {
+            done = true
+        }
+        else -> throw RuntimeException("Unknown opcode in instruction: ${mem[pc]}, pc: $pc, mem: $mem")
     }
-    2 -> { // MUL
-        mem[mem[pc + 3]] = getParam(0) * getParam(1); pc += 4; this
-    }
-    3 -> { // INPUT
-        mem[mem[pc + 1]] = input; pc += 2; this
-    }
-    4 -> { // OUTPUT
-        output.add(getParam(0)); pc += 2; this
-    }
-    99 -> {
-        done = true; this
-    }
-    else -> throw RuntimeException("Unknown opcode: ${mem[pc]}, pc: $pc, mem: $mem")
+    return this
+}
+
+fun State.setParam(index: Int, value: Int): State = apply {
+    mem[mem[pc + index]] = value
 }
 
 fun State.getParam(index: Int): Int = when (val mode = mem[pc].parameterMode(index)) {
-    0 -> mem[mem[pc + index + 1]]
-    1 -> mem[pc + index + 1]
+    0 -> mem[mem[pc + index]]
+    1 -> mem[pc + index]
     else -> throw RuntimeException("Unknown parameter mode: $mode, pc: $pc, mem: $mem")
 }
 
 fun Int.opcode(): Int = this % 100
 fun Int.parameterMode(paramIndex: Int): Int {
     var mode = this
-    repeat(paramIndex + 2) { mode /= 10 }
+    repeat(paramIndex + 1) { mode /= 10 }
     return mode % 10
 }
