78fd52d8f4349731c5a0c89bcc4380a1a4c90fb2
Christoph Dietze <git@cdietze.de>
2020-12-06 13:26:20 +0100

    Solve day 6

---
+++ 2020/src/main/kotlin/advent2020/day06/Main.kt
@@ -0,0 +1,23 @@
+package advent2020.day06
+
+import advent2020.AdventDay
+
+object Main : AdventDay {
+    private val inputText by lazy { this.javaClass.getResource("input.txt").readText() }
+
+    @JvmStatic
+    fun main(args: Array<String>) = run()
+
+    override fun resultPart1(): Any = resultPart1(inputText)
+    override fun resultPart2(): Any = resultPart2(inputText)
+}
+
+fun resultPart1(input: String): Long =
+    input.split("\n\n").map { group ->
+        group.filter { c -> ('a'..'z').contains(c) }.toSet().size.toLong()
+    }.sum()
+
+fun resultPart2(input: String): Long =
+    input.split("\n\n").map { group ->
+        group.lines().map { it.toSet() }.reduce { a, b -> a.intersect(b) }.size.toLong()
+    }.sum()
