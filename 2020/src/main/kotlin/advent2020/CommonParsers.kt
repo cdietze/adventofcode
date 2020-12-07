d8d51ad13fea7f2c5840bb1648771c7f2d525e99
Christoph Dietze <git@cdietze.de>
2020-12-07 22:37:54 +0100

    Solve day 7

--- 2020/src/main/kotlin/advent2020/CommonParsers.kt
+++ 2020/src/main/kotlin/advent2020/CommonParsers.kt
@@ -1,15 +1,9 @@
 package advent2020
 
-import parsek.CharIn
-import parsek.Parser
-import parsek.Rule
-import parsek.WhileCharIn
-import parsek.capture
-import parsek.map
-import parsek.opt
-import parsek.times
+import parsek.*
 
 object CommonParsers {
     val int: Parser<Int> = Rule("int") { (CharIn("+-").opt() * WhileCharIn("0123456789")).capture().map { it.toInt() } }
     val space = Rule("space") { WhileCharIn(" \r\n", min = 0) }
+    val word: Parser<String> = Rule("word") { WhileCharIn(('a'..'z') + ('A'..'Z')) }.capture()
 }
