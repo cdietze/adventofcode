d3b8fd798c5b37a82e602c3e246dcf8aeb50a991
Christoph Dietze <git@cdietze.de>
2020-12-18 22:47:23 +0100

    Solve day 18

--- 2020/src/main/kotlin/advent2020/CommonParsers.kt
+++ 2020/src/main/kotlin/advent2020/CommonParsers.kt
@@ -4,6 +4,8 @@ import parsek.*
 
 object CommonParsers {
     val int: Parser<Int> = Rule("int") { (CharIn("+-").opt() * WhileCharIn("0123456789")).capture().map { it.toInt() } }
+    val long: Parser<Long> =
+        Rule("long") { (CharIn("+-").opt() * WhileCharIn("0123456789")).capture().map { it.toLong() } }
     val space = Rule("space") { WhileCharIn(" \r\n", min = 0) }
     val word: Parser<String> = Rule("word") { WhileCharIn(('a'..'z') + ('A'..'Z')) }.capture()
 }
