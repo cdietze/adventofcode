4c32038e2f115cc0e4731c31a17b529c9c988981
Christoph Dietze <git@cdietze.de>
2020-12-24 23:56:48 +0100

    Solve day 23 part 1

---
+++ 2020/src/test/kotlin/advent2020/Day23Test.kt
@@ -0,0 +1,13 @@
+package advent2020
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day23Test {
+
+    @Test
+    fun testPart1() {
+        assertEquals("67384529", advent2020.day23.resultPart1("389125467"))
+    }
+
+}
