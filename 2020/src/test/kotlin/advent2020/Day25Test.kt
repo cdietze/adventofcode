5c4b29bb5d2638b219dec533f6ac854f64c5af44
Christoph Dietze <christoph.dietze@kurzdigital.com>
2020-12-25 20:59:20 +0100

    Solve day 25 part 1

---
+++ 2020/src/test/kotlin/advent2020/Day25Test.kt
@@ -0,0 +1,12 @@
+package advent2020
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day25Test {
+
+    @Test
+    fun testPart1() {
+        assertEquals(14897079L, advent2020.day25.resultPart1(5764801L, 17807724L))
+    }
+}
