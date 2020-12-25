63ff0946daf1fbae60ee8b7ebf3cd3f2999fd5a5
Christoph Dietze <git@cdietze.de>
2020-12-25 21:55:51 +0100

    Solve day 24

---
+++ 2020/src/test/kotlin/advent2020/Day24Test.kt
@@ -0,0 +1,38 @@
+package advent2020
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day24Test {
+
+    @Test
+    fun testPart1() {
+        assertEquals(10, advent2020.day24.resultPart1(demoInput))
+    }
+
+    @Test
+    fun testPart2() {
+        assertEquals(2208, advent2020.day24.resultPart2(demoInput))
+    }
+
+    val demoInput = """sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"""
+}
